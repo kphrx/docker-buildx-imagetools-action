3f9b9c23ea7a22e8b26256d42b323660f6c9ccad
kPherox <kphrx@kpherox.dev>
2025-05-08 08:04:52 +0000

    style: remove no longer used post script function

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -3,8 +3,3 @@
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export declare function run(): Promise<void>;
-/**
- * The post function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-export declare function cleanup(): Promise<void>;
--- src/main.ts
+++ src/main.ts
@@ -137,9 +137,3 @@ async function main(): Promise<void> {
     if (e instanceof Error) core.info(e.message)
   }
 }
-
-/**
- * The post function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-export async function cleanup(): Promise<void> {}
